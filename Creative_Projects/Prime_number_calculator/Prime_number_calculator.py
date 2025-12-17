314f508da7d6a580584a371796093967ca50dafa
Robert <robertbavisotto@gmail.com>
2025-12-17 15:59:58 +0100

    Add files via upload

---
+++ Creative_Projects/Prime_number_calculator/Prime_number_calculator.py
@@ -0,0 +1,31 @@
+distance_list = []
+prime_list = []
+ntemp = 0
+
+def is_prime(num):
+    for i in range(2, int(1 + num ** 0.5)):
+        if num % i == 0:
+            return False
+    return True
+
+for i in range(1, 5000):
+    if is_prime(i + 1):
+        #print(i + 1, end=" ")
+        prime_list.append(i+1)
+
+print(prime_list)
+
+
+for n in prime_list:
+    if n >= 3:
+        distance = n - ntemp
+    else:
+        distance = n - 1
+    distance_list.append(distance)
+    ntemp = n
+
+#print(distance_list)
+
+with open('Prime_number.txt', 'w') as f:
+    for line in prime_list:
+        f.write(f"{line}\n")
