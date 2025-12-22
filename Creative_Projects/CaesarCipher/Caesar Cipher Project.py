d2fc9b8427da250ee346d0928a1e622b6f131fc7
Robert <robertbavisotto@gmail.com>
2025-12-22 12:58:11 +0100

    Update Caesar Cipher Project.py

--- Creative_Projects/CaesarCipher/Caesar Cipher Project.py
+++ Creative_Projects/CaesarCipher/Caesar Cipher Project.py
@@ -3,6 +3,8 @@
 
 #The following project has an input message that 
 
+#_____________________________Section 1________________________________
+# This section introduces the fundamental concept of caesar ciphers 
 def caesar_cipher(message, shift):
     alphabet = list("abcdefghijklmnopqrstuvwxyz")
     result = ""
@@ -32,6 +34,107 @@ def caesar_cipher(message, shift):
 # Example usage
 text = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
 
-print(caesar_cipher(text, 10)) 
+#print(caesar_cipher(text, 10)) 
+#calls the function to decode the text with known shift of 10
+
+reply = "This is a test of my caesar cipher project. Lets see if it works! (10)"
+
+#print(caesar_cipher(reply, 10))
+#Calls the function to encrype the message using a shift of 10
 
 
+
+#___________________________Section 2 __________________________________
+#Example of brute force decryption
+
+Vishal1 = "jxu evviuj veh jxu iusedt cuiiqwu yi vekhjuud."
+
+Vishal2 = "bqdradyuzs ygxfubxq omqemd oubtqde fa oapq kagd yqeemsqe ue qhqz yadq eqogdq!"
+
+#print (caesar_cipher(Vishal1, 10))
+#print (caesar_cipher(Vishal2, 14))
+
+Vishal3 = "vhfinmxkl atox kxgwxkxw tee hy maxlx hew vbiaxkl hulhexmx. px'ee atox mh kxteer lmxi ni hnk ztfx by px ptgm mh dxxi hnk fxlltzxl ltyx." 
+
+#print (caesar_cipher(Vishal3, 1)+ "   shift = 1")
+#print (" ")
+#print (caesar_cipher(Vishal3, 2)+ "   shift = 2") 
+#print (" ")
+#print (caesar_cipher(Vishal3, 3)+ "   shift = 3") 
+#print (" ")
+#print (caesar_cipher(Vishal3, 4)+ "   shift = 4") 
+#print (" ")
+#print (caesar_cipher(Vishal3, 5)+ "   shift = 5") 
+#print (" ")
+#print (caesar_cipher(Vishal3, 6)+ "   shift = 6") 
+#print (" ")
+#print (caesar_cipher(Vishal3, 7)+ "   shift = 7") 
+#print (" ")
+
+#___________________________Section 3_______________________________
+#Second section Vigenere Cipher
+# A polyalphabetic substitution cipher
+# Invented by an Italian Cryptologist named Giovan Battista Bellaso
+
+def vigenere_decode(ciphertext, keyword):
+    result = []
+    key_index = 0
+    keyword = keyword.lower()
+
+    for char in ciphertext:
+        if char.isalpha():
+            base = ord('a') if char.islower() else ord('A')
+
+            c = ord(char.lower()) - ord('a')
+            k = ord(keyword[key_index % len(keyword)]) - ord('a')
+
+            # NOTE: add the key value to decode this message
+            p = (c + k) % 26
+            result.append(chr(p + base))
+
+            key_index += 1
+        else:
+            result.append(char)
+
+    return ''.join(result)
+
+
+if __name__ == "__main__":
+    keyword = "friends"
+    message = (
+        "txm srom vkda gl lzlgzr qpdb? fepb ejac! "
+        "ubr imn tapludwy mhfbz cza ruxzal wg zztylktoikqq!"
+    )
+
+    print(vigenere_decode(message, keyword))
+    
+#_____________________________Section 4___________________________
+def vigenere_encode(plaintext, keyword):
+    result = []
+    key_index = 0
+    keyword = keyword.lower()
+
+    for char in plaintext:
+        if char.isalpha():
+            base = ord('a') if char.islower() else ord('A')
+
+            p = ord(char.lower()) - ord('a')
+            k = ord(keyword[key_index % len(keyword)]) - ord('a')
+
+            # NOTE: subtract key to match the earlier cipher
+            c = (p - k) % 26
+            result.append(chr(c + base))
+
+            key_index += 1
+        else:
+            result.append(char)
+
+    return ''.join(result)
+
+
+if __name__ == "__main__":
+    keyword = input("Please enter a keyword: ")
+    message = "You can now encode messages using this cipher!"
+
+    encoded_message = vigenere_encode(message, keyword)
+    print(encoded_message)
