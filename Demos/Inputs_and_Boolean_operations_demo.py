9dd948255bf9b4056a810fe141b40d725832c400
Robert <robertbavisotto@gmail.com>
2026-02-22 09:21:22 +0100

    Delete Demos directory

--- Demos/Inputs_and_Boolean_operations_demo.py
+++
@@ -1,5 +0,0 @@
-# This project is to learn how to perform type inputs from users, & simple Boolean operations
-# Code by Robert Bavisotto
-
-n = int(input("Please enter a number:"))
-print(n >= 100)
--- Demos/Time_counting_demo.py
+++
@@ -1,7 +0,0 @@
-import time
-
-for i in range (1,10):
-    print(i, "Mississippi")
-    time.sleep(1)
-
-print("Ready or not, here I come!")
